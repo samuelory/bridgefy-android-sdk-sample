e77d46630efa84fa1ae024b6e4da5fbe7fab7ccf
David Álvarez <dalvar.lopez@gmail.com>
2017-11-28 07:46:00 -0600

    fixed permission results

--- twitter/src/main/java/com/bridgefy/samples/twitter/IntroActivity.java
+++ twitter/src/main/java/com/bridgefy/samples/twitter/IntroActivity.java
@@ -158,7 +158,7 @@ public class IntroActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             // Start Bridgefy
             startBridgefy();
 
